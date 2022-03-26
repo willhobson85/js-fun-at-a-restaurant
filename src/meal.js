c862e6fc536563c46d65405d4a8507efdae87b97
William Hobson <will.hobson85@gmail.com>
2022-03-26 16:16:57 -0400

    Complete meal

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,45 @@
-function nameMenuItem() {
-
+function nameMenuItem(foodType) {
+return `Delicious ${foodType}`
 }
 
+function createMenuItem(itemName, itemPrice, itemType) {
+    return {
+    name: itemName,
+    price: itemPrice,
+    type: itemType
+  }
+}
+
+function addIngredients(newIngredient, ingredients) {
+  if (ingredients.includes(newIngredient)) {
+    return ingredients
+  } else {
+    return ingredients.push(newIngredient);
+  }
+}
+
+function formatPrice(initialPrice) {
+  return `$${initialPrice}`
+}
+
+function decreasePrice(discount) {
+  var salePrice = (discount * .90);
+  return salePrice
+}
+
+function createRecipe(comboName, ingredients, whichMenu) {
+return {
+  title: comboName,
+  ingredients: ingredients,
+  type: whichMenu
+  }
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
