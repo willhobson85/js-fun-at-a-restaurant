03af23e0d352bf86fb34e6590dcdb75aafb9f631
William Hobson <will.hobson85@gmail.com>
2022-03-26 23:21:45 -0400

    Complete Order

--- src/order.js
+++ src/order.js
@@ -1,7 +1,38 @@
+function takeOrder(orderNumber, typeOfOrder) {
+  if (typeOfOrder.length < 3) {
+    return typeOfOrder.push(orderNumber)
+  }
+    return typeOfOrder
+}
+
+function refundOrder(ordNumber, arrayOrder) {
+  for (var i = 0; i < arrayOrder.length; i++) {
+    if (ordNumber === arrayOrder[i].orderNumber) {
+          arrayOrder.splice(i, 1)
+        }
+    }
+}
+
+function listItems(deliveryOrders) {
+  var entree = [];
+    for (var i = 0; i < deliveryOrders.length; i++) {
+        entree.push(deliveryOrders[i].item);
+    }
+    return entree.join(', ');
+}
+
+function searchOrder(deliveryOrders, food) {
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].item === food) {
+      return true
+    }
+  }
+      return false
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
